985594648e9963d2aa13a24a353114baf2637b92
ramsteven <ramsteven@gmail.com>
2020-10-28 01:42:00 +0000

    adding task 1

--- test_1.c
+++ test_1.c
@@ -6,9 +6,8 @@
  **/
 int _printf(char *format, ...)
 {
-	int CharacterCount = 0;
+	int CharacterCount = 0, i = 0;
 	va_list Valist;
-	int i = 0;
 
 	va_start(Valist, format);
 	if (!format || (format[0] == '%' && _strl(format) == 1))
@@ -17,8 +16,7 @@ int _printf(char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			switch (format[i])
+			switch (format[i + 1])
 			{
 			case 's':
 				CharacterCount += _string(va_arg(Valist, char*));
@@ -35,7 +33,6 @@ int _printf(char *format, ...)
 				break;
 			default:
 				CharacterCount += _character('%');
-				CharacterCount += _character(format[i]);
 				break;
 			}
 		}
